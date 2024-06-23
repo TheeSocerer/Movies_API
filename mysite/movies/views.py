88c63785abfb96664c761379b5d3b81fa21236c0
tshiburi123 <tshiburi123@student.wethinkcode.co.za>
2024-06-23 21:24:54 +0200

    added a url route of the api and also created a view

--- mysite/movies/views.py
+++ mysite/movies/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .serislizers import MovieSerializer
+from .models import Moviedata
+
+
+class MovieViewSet(viewsets.ModelViewSet):
+    queryset = Moviedata.objects.all()
+    serializer_class = MovieSerializer
+
+
 
 # Create your views here.
